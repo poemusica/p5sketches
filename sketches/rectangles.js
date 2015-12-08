9d0414e319951fe86f9f117a780f0be319a19c1a
Heather Mahan <heatheramahan@gmail.com>
2015-12-08 10:01:01 -0800

    Uses color harmonizer function for colors.

--- sketches/rectangles.js
+++ sketches/rectangles.js
@@ -7,6 +7,7 @@ var sketch = function (p) {
             width: 50,
             height: 100,
             colors: [],
+            alpha: 0.5
         },
         apiCall,
         apiSrcColour = 'http://www.colourlovers.com/api/palettes/random?format=json&jsonCallback=sketch.parseColours',
@@ -21,17 +22,14 @@ var sketch = function (p) {
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
         p.ellipseMode(p.CORNER);
-        //init();
         body = document.getElementsByTagName('body')[0];
-        getColors();
+        init();
     };
 
     p.draw = function () {
-        p.background(0);
-
         for (var i = 0; i < positions.length; i++) {
             p.noStroke();
-            p.stroke(strokes[i]);
+            //p.stroke(strokes[i]);
             p.fill(colors[i]);
             p.push();
             p.translate(positions[i].x + config.width/2, positions[i].y + config.height/2);
@@ -50,39 +48,75 @@ var sketch = function (p) {
     };
 
     function init() {
+        var referenceAngle = p.random(0, 360),
+            y = -config.height,
+            stepx = 0.01,
+            stepy = 100;
+        setPosAndRot();
+        while (y < p.windowHeight + config.height) {
+            var x = 0,
+                referenceColor = colorHarmonizer(referenceAngle);
+            while(x < p.windowWidth) {
+                colors.push( perlinLightness(referenceColor, x, y) );
+                p.colorMode(p.RGB);
+                strokes.push( p.lerpColor(p.color(255), p.color(0), p.noise(x, y)) );
+                x += config.width * 0.25;
+                stepx += 0.001;
+            }
+            y += config.height * 0.25;
+            stepy += 0.001;
+        }
+    }
+
+    // Returns an array of positions based on window dimensions.
+    function setPosAndRot() {
         var y = -config.height,
             stepx = 0.01,
             stepy = 100,
-            prevR = p.random(-p.PI/12, p.PI/12),
-            prevC = white;
-            i = 0;
-        positions = [];
-        rotations = [];
-        colors = [],
-        strokes = [];
-        mapping.c2 = complementHSL(mapping.c1);
+            prevR = p.random(-p.PI/12, p.PI/12);
         while (y < p.windowHeight + config.height) {
             var x = 0;
             while(x < p.windowWidth) {
-                var newR = prevR + p.map(p.noise(stepx, stepy), 0, 1, -p.PI/6, p.PI/6),
-                    index = p.round(p.map(p.noise(x/100, y/100), 0, 1, 0, config.colors.length - 1)),
-                    newC = p.lerpColor(config.colors[index], prevC, 0.8);
+                var newR = prevR + p.map(p.noise(stepx, stepy), 0, 1, -p.PI/6, p.PI/6);
                 rotations.push(newR);
                 positions.push( new p5.Vector(x, y) );
-                colors.push( newC );
-                strokes.push( p.lerpColor( black, white, p.saturation(colors[colors.length - 1])/100 ) );
                 x += config.width * 0.25;
                 stepx += 0.001;
                 prevR = newR;
             }
             y += config.height * 0.25;
             stepy += 0.001;
-            prevC = config.colors[ p.floor(i % (config.colors.length - 1))];
-            i++;
         }
     }
 
     // COLOR 
+
+    // Returns a harmonized HSL color based on reference angle input.
+    // Adapted from http://devmag.org.za/2012/07/29/how-to-choose-colours-procedurally-algorithms/
+    function colorHarmonizer(referenceAngle) {
+        var result,
+            // Analogous: Choose second and third ranges 0.
+            // Complementary: Choose rangeAngle2 = 0, offsetAngle1 = 180.
+            // Split Complementary: Choose offset angles 180 +/- a small angle. The second and third ranges must be smaller than the difference between the two offset angles.
+            // Triad: Choose offset angles 120 and 240.
+            offsetAngle1 = 120, 
+            offsetAngle2 = 240,
+            rangeAngle0 = 15, 
+            rangeAngle1 = 30, 
+            rangeAngle2 = 60,
+            rangeAngleSum = rangeAngle0 + rangeAngle1 + rangeAngle2,
+            saturation = p.random(60, 100), 
+            luminance = p.random(50, 70),
+            randomAngle;
+        p.colorMode(p.HSL, 360, 100, 100);
+        randomAngle = p.random(0, rangeAngleSum);
+        if (randomAngle > rangeAngle0) {
+            if (randomAngle < rangeAngle0 + rangeAngle1) {
+                randomAngle += offsetAngle1;
+            } else { randomAngle += offsetAngle2; }
+        }
+        return p.color( (referenceAngle + randomAngle) % 360, saturation, luminance);
+    }
 
     function getColors() {
         var oldCall = document.getElementById('color-script');
@@ -113,6 +147,13 @@ var sketch = function (p) {
             return p.lerpColor(white, mapping.c1, p.map(amt, 0, 0.5, 0, 1));
         }
         return p.lerpColor(white, mapping.c2, p.map(amt, 0.5, 1, 0, 1));
+    }
+
+        // Takes a color and two variables. Returns a color based on output of perlin noise.
+    function perlinLightness(c, x, y) {
+        var amt = p.noise(x, y);
+        p.colorMode(p.HSL);
+        return p.color( p.hue(c), p.saturation(c), p.map(amt, 0, 1, -15, 15) + p.lightness(c), config.alpha );
     }
 
     function complementHSL(c) {
