c9633620372374fa5428146e4136410ecc1fbe3a
Heather Mahan <heatheramahan@gmail.com>
2015-12-29 13:24:44 -0800

    Add toggle UI for tidy and shuffle. Code refactor.

--- sketches/chroma-test.js
+++ sketches/chroma-test.js
@@ -1,42 +1,55 @@
 var sketch = function (p) {
-    var positions,
+    var 
+        // Render arrays
+        positions,
         rotations,
         palette,
-        colors,
+        // Config data
         config = {
-            width: 400,
-            height: 75,
-            colorA: chroma('#FFFFFF'),
-            colorB: chroma('#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6)),
-            colorC: chroma('#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6)),
-            colorD: chroma('#000000'),
+            width: null,
+            height: null,
+            chromaA: null,
+            chromaB: null,
+            chromaC: null,
+            chromaD: null,
         },
+        // GUI and GUI data
+        gui,
         data = {
-            colorA: config.colorA.hex(),
-            colorB: config.colorB.hex(),
-            colorC: config.colorC.hex(),
-            colorD: config.colorD.hex(),
+            swatchA: '#FFFFFF',
+            swatchB: '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6),
+            swatchC: '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6),
+            swatchD: '#000000',
             randomize: function() { randomize(); p.loop(); },
-            sort: function() { colors = setColors(positions.length); p.loop();},
-            shuffle: function () { shuffleArray(colors); p.loop();},
-            tidy: function() { centerPositions(); rotations = resetRotations(positions.length); p.loop();},
-            messy: function() { positions = setPositions(); rotations = setRotations(positions.length); p.loop();},
-        },
-        gui;
+            shuffle: false,
+            tidy: false,
+        };
 
     p.setup = function () {
+        var guiElt,
+            a, b, c, d;
+        // Set up sketch.
         p.createCanvas(p.windowWidth, p.windowHeight);
         p.rectMode(p.CENTER);
+        // Set up config width and height.
+        config.width = p.max(p.width/3, 200);
+        config.height = p.max(p.height/8, 45);
+        // Set up render arrays.
+        positions = setPositions();
+        rotations = setRotations(positions.length);
+        palette = setPalette();
+        // Set up GUI in DOM.
         gui = new dat.GUI( { autoPlace: false } );
-        var guiElt = gui.domElement;
+        guiElt = gui.domElement;
         document.getElementById('p5-sketch').appendChild(guiElt);
         guiElt.style.position = 'fixed';
         guiElt.style.left = '0px';
         guiElt.style.top = '0px';
-        var a = gui.addColor(data, 'colorA').name("Color");
-            b = gui.addColor(data, 'colorB').name("Color");
-            c = gui.addColor(data, 'colorC').name("Color");
-            d = gui.addColor(data, 'colorD').name("Color");
+        // Set up GUI color swatches.
+        a = gui.addColor(data, 'swatchA').name("Color");
+        b = gui.addColor(data, 'swatchB').name("Color");
+        c = gui.addColor(data, 'swatchC').name("Color");
+        d = gui.addColor(data, 'swatchD').name("Color");
         a.onChange( function() { updatePalette(); p.loop(); } );
         a.onFinishChange(function(){ sortUI(); p.loop(); });
         b.onChange( function() { updatePalette(); p.loop(); } );
@@ -45,23 +58,33 @@ var sketch = function (p) {
         c.onFinishChange(function(){ sortUI(); p.loop(); });
         d.onChange( function() { updatePalette(); p.loop(); } );
         d.onFinishChange(function(){ sortUI(); p.loop(); });
-        gui.add(data,'randomize').name('Get Random');
-        gui.add(data,'sort').name('Sort Palette');
-        gui.add(data,'shuffle').name('Shuffle Palette');
-        gui.add(data,'tidy').name('Tidy Palette');
-        gui.add(data,'messy').name('Messy Palette');
-        positions = setPositions();
-        rotations = setRotations(positions.length);
-        reorder();
-        palette = setPalette();
-        colors = setColors(positions.length);
+        // Set up new palette button.
+        gui.add(data,'randomize').name('New Palette');
+        // Set up toggles.
+        gui.add(data, 'shuffle').name('Shuffle').onFinishChange( function() {
+            if (data.shuffle) { palette = shuffleArray(palette); }
+            else { palette = sortChromas(palette); }
+            p.loop();
+        });
+        gui.add(data,'tidy').name('Tidy').onFinishChange( function() {
+            if (data.tidy) { 
+                positions = centerPositions(positions); 
+                rotations = resetRotations(positions.length);
+            } else { 
+                positions = setPositions(); 
+                rotations = setRotations(positions.length);
+            }
+            p.loop();
+        });
+        // Sort GUI color swatches.
+        sortUI();
     };
 
     p.draw = function () {
         p.background('#DCDCDC');
         for (var i = 0; i < positions.length; i++) {
             p.noStroke();
-            p.fill(colors[i]);
+            p.fill(palette[i]);
             p.push();
             p.translate(positions[i].x, positions[i].y);
             p.rotate(rotations[i]);
@@ -73,8 +96,12 @@ var sketch = function (p) {
 
     p.windowResized = function () {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
+        config.width = p.max(p.width/3, 200);
+        config.height = p.max(p.height/8, 45);
         positions = setPositions();
+        if (data.tidy) { positions = centerPositions(positions); }
         rotations = setRotations(positions.length);
+        if (data.tidy) { rotations = resetRotations(positions.length); }
         updatePalette();
         p.loop();
     };
@@ -83,7 +110,7 @@ var sketch = function (p) {
     function setPositions() {
         var result = [],
             x = p.width/2,
-            y = config.height;
+            y = config.height * 1.25;
         while (y < p.windowHeight - config.height) {
             var xoff = p.random(-50, 50);
             result.push( new p5.Vector(x + xoff, y) );
@@ -92,11 +119,13 @@ var sketch = function (p) {
         return result;
     }
 
-    // Centers x-values in positions array based on window size.
-    function centerPositions() {
-        for (var i = 0; i < positions.length; i++) {
-            positions[i].x = p.width/2;
-        } 
+    // Returns an x-axis centered version of input vector array based on sketch width.
+    function centerPositions(array) {
+        var result = array.slice();
+        for (var i = 0; i < result.length; i++) {
+            result[i].x = p.width/2;
+        }
+        return result;
     }
 
     // Returns an array of rotations of specified length.
@@ -117,42 +146,12 @@ var sketch = function (p) {
         return result;
     }
 
-    // Returns array of colors from chroma.
-    function setPalette() {
-        config.colorA = chroma(data.colorA);
-        config.colorB = chroma(data.colorB);
-        config.colorC = chroma(data.colorC);
-        config.colorD = chroma(data.colorD);
-        var l = [config.colorA, config.colorB, config.colorC, config.colorD];
-        l.sort(function(a, b) {
-          return b.get('hcl.l') - a.get('hcl.l');
-        });
-        var bezInterpolator = chroma.bezier([l[0], l[1], l[2], l[3]])
-        return chroma.scale(bezInterpolator).padding(0).correctLightness(true).colors(positions.length);
-    }
-
-    // Returns an array of colors of specified length.
-    function setColors(n) {
-        var result = [];
-        for (var i = 0; i < n; i++) {
-            var c = palette[i];
-            result.push(c);
-        }
-        return result;
-    }
-
-
-    function updatePalette() {
-        palette = setPalette();
-        colors = setColors(positions.length);
-    }
-
-    // Generate new random palette of colors.
+    // Generate new random swatches and palette.
     function randomize() {
-        data.colorA = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
-        data.colorB = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
-        data.colorC = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
-        data.colorD = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
+        data.swatchA = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
+        data.swatchB = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
+        data.swatchC = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
+        data.swatchD = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6);
         for (var i in gui.__controllers) {
             gui.__controllers[i].updateDisplay();
         }
@@ -160,20 +159,53 @@ var sketch = function (p) {
         sortUI();
     }
 
-    // Shuffle an array in place using Fisher-Yates method.
-    function shuffleArray (array) {
-        var i = 0, 
-            j = 0,
-            temp = null;
-        for (var i = array.length - 1; i > 0; i -= 1) {
-            j = Math.floor(Math.random() * (i + 1));
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
+    // Returns array of hex colors from chroma scale.
+    function setPalette() {
+        var l = [],
+            bezInterpolator = null;
+        config.chromaA = chroma(data.swatchA);
+        config.chromaB = chroma(data.swatchB);
+        config.chromaC = chroma(data.swatchC);
+        config.chromaD = chroma(data.swatchD);
+        l = [config.chromaA, config.chromaB, config.chromaC, config.chromaD];
+        l.sort(function(a, b) {
+          return b.get('hcl.l') - a.get('hcl.l');
+        });
+        bezInterpolator = chroma.bezier([l[0], l[1], l[2], l[3]])
+        return chroma.scale(bezInterpolator).padding(0).correctLightness(true).colors(positions.length);
     }
 
-    // Rearrange color list items in DOM according to lightness.
+    // Returns a sorted version of input chroma array.
+    function sortChromas(array) {
+        var result = array.slice();
+        result.sort(function(a, b) {
+          return chroma(b).get('hcl.l') - chroma(a).get('hcl.l');
+        });
+        return result;
+    }
+
+    // Reassigns palette and shuffles, if needed.
+    function updatePalette() {
+        palette = setPalette();
+        if (data.shuffle) { palette = shuffleArray(palette); }
+    }
+
+    // Returns a shuffled version of input array using Fisher-Yates method.
+    function shuffleArray (array) {
+        var result = array.slice(),
+            i = 0, 
+            j = 0,
+            temp = null;
+        for (var i = result.length - 1; i > 0; i -= 1) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+        }
+        return result;
+    }
+
+    // Rearrange swatch list items in DOM according to lightness.
     function sortUI() {
         var items = document.getElementsByClassName('color'),
             ul = items[0].parentElement,
@@ -183,27 +215,12 @@ var sketch = function (p) {
             itemList.push(items[i]); 
         }
         itemList.sort( function(a, b) {
-            var colorA = chroma(a.getElementsByClassName('c')[0].firstChild.value),
-                colorB = chroma(b.getElementsByClassName('c')[0].firstChild.value);
-            return colorB.get('hcl.l') - colorA.get('hcl.l');
+            var chromaA = chroma(a.getElementsByClassName('c')[0].firstChild.value),
+                chromaB = chroma(b.getElementsByClassName('c')[0].firstChild.value);
+            return chromaB.get('hcl.l') - chromaA.get('hcl.l');
         });
         for (var i = 0; i < itemList.length; i++) {
             ul.insertBefore(itemList[i], button);
-        }
-    }
-
-    // Rearrange data colors according to lightness.
-    function reorder() {
-        var l = [chroma(data.colorA), chroma(data.colorB), chroma(data.colorC), chroma(data.colorD)];
-            l.sort(function(a, b) {
-            return b.get('hcl.l') - a.get('hcl.l');
-        });
-        data.colorA = l[0].hex();
-        data.colorB = l[1].hex();
-        data.colorC = l[2].hex();
-        data.colorD = l[3].hex();
-        for (var i in gui.__controllers) {
-            gui.__controllers[i].updateDisplay();
         }
     }
 
