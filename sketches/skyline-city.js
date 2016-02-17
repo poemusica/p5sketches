cb1ec607ae344cf9787a6c3934d70c7e00e3e2c4
Heather Mahan <heatheramahan@gmail.com>
2016-02-17 13:06:21 -0800

    Cap, base, body partitions. 4 building body patterns.

--- sketches/skyline-city.js
+++ sketches/skyline-city.js
@@ -2,6 +2,9 @@ var sketch = function (p) {
     var scale,
         bg,
         fg,
+        // patterns = ['gridOfHBands', 'hStripes', 'vStripes'],
+        patterns = ['grid', 'hBands', 'vBands', 'rowsOfGrids'],
+        drawPattern,
         stars;
 
     p.setup = function () {
@@ -62,57 +65,136 @@ var sketch = function (p) {
         }
         // CLOSE BUILDINGS
         for (var i = 0; i < 10; i++) {
-            var x = p.random(-50, p.width - 150),
-                y = p.height,
-                w = p.random(100, 300),
-                h = p.random(p.height * 0.65, p.height/6);
-            graphic.fill(0);
-            graphic.stroke(scale(0.5).rgb());
-            graphic.strokeWeight(3);
-            graphic.rect(x, y, w, -h);
-
-            var chance = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-
-            switch (chance) {
-                case 1:
-                // VERTICAL LINES WITH CAP
-                    var n = Math.floor(Math.random() * (15 - 10 + 1)) + 10,
-                        cap = p.random(h * 0.85, h * 0.9);
-                    for (var j = 0; j < n; j++) {
-                        graphic.line(x + w/n * j, y, x + w/n * j, y - cap)
-                    }
-                    graphic.line(x, y - cap, x + w, y - cap);
-                    graphic.fill(scale(0.5).darken().rgb());
-                    graphic.rect(x, y - h, w, h - cap);
-                    break;
-                case 2:
-                    // HORIZONTAL LINES WITH ALTERNATING FILL
-                    var n = Math.floor(Math.random() * (h/25 - h/15 + 1)) + Math.floor(h/15);
-                    graphic.fill(scale(0.5).darken().rgb());
-                    for (var j = 0; j < n; j++) {
-                        graphic.line(x, y - h/n * j, x + w, y -h/n * j);
-                        if (j % 2) { graphic.rect(x, y - h/n * j, w, -h/n); }
-                    }
-                    break;
-                case 3:
-                    // WINDOW GRID
-                    var cols = Math.floor(p.random(3, 8)),
-                        stories = Math.floor(Math.random() * (h/25 - h/15 + 1)) + Math.floor(h/15),
-                        winH = Math.floor(h/stories/2),
-                        winW = Math.floor(w/cols/2),
-                        xmargin = winW;
-                    graphic.fill(scale(0.5).rgb());
-                    for (var j = 0; j < stories; j++) {
-                        for ( var k = 0; k < cols; k++) {
-                            graphic.rect(x + xmargin/2 + w/cols * k, y - h/stories * j, winW, winH);
-                        }
-                    }
-                    break;
-            }
+            drawBuilding(graphic);
         }
         return graphic;
-
     }
+
+    function drawBuilding(graphic) {
+        var lowerLeft = p.createVector(p.random(-50, p.width - 150), p.height),
+            w = p.random(100, 300),
+            h = p.random(p.height * 0.75, p.height/6),
+            cap = { bool: false, height: 0, pattern: undefined },
+            body = { height: 0, pattern: undefined },
+            base = { bool: false, height: 0, pattern: undefined };
+        if (Math.random() > 0.5) {
+            cap.bool = true;
+            cap.height = p.ceil(h * (p.random(15, 30)/100));
+        }
+        if (Math.random() > 0.5) {
+            base.bool = true;
+            base.height = p.ceil(h * (p.random(10, 20)/100));
+        }
+        body.width = w;
+        body.height = h - cap.height - base.height;
+        body.pattern = patterns[Math.floor(Math.random() * patterns.length)];
+        body.lowerLeft = p.createVector(lowerLeft.x, lowerLeft.y - base.height);
+
+        // OUTLINES
+        graphic.fill(0);
+        graphic.stroke(scale(0.5).rgb());
+        graphic.strokeWeight(3);
+        graphic.rect(lowerLeft.x, lowerLeft.y - base.height, w, -body.height);
+        if (cap.bool) { graphic.rect(lowerLeft.x, lowerLeft.y - h, w, cap.height); }
+        if (base.bool) { graphic.rect(lowerLeft.x, lowerLeft.y, w, -base.height); }
+        drawPattern[body.pattern](body, graphic);
+    }
+
+    drawPattern = {
+        rowsOfGrids: function(component, graphic) {
+            var rows = 3,
+                space = Math.floor(p.random(20, 30)),
+                size = space - Math.floor(p.random(5, 10)),
+                margin = p.random(10, 30);
+            for (var i = 0; i < rows; i++) {
+                var body = {
+                        width: component.width,
+                        height: component.height/3,
+                        lowerLeft: p.createVector(component.lowerLeft.x, component.lowerLeft.y - (component.height/3) * i),
+                        space: space,
+                        size: size,
+                        margin: margin,
+                    };
+                this.grid(body, graphic);
+                graphic.line(body.lowerLeft.x, body.lowerLeft.y, body.lowerLeft.x + body.width, body.lowerLeft.y);
+            }
+        },
+        grid: function(component, graphic) {
+            var space = component.space || Math.floor(p.random(20, 30)),
+                size = component.size || space - Math.floor(p.random(5, 10)),
+                margin = component.margin || p.random(10, 30),
+                xm = Math.ceil((component.width - margin) / space),
+                ym = Math.ceil((component.height - margin) / space),
+                h = component.height,
+                w = component.width - xm/2,
+                m = 0;
+            graphic.stroke(scale(0.5).darken().rgb());
+            // graphic.rect(component.lowerLeft.x + margin/2, component.lowerLeft.y - margin/2, component.width - margin, -component.height + margin);
+            graphic.push();
+            graphic.translate(component.lowerLeft.x, component.lowerLeft.y - component.height);
+            graphic.rectMode(graphic.CENTER);
+            graphic.fill(scale(0.5).darken().rgb());
+            for (var y = h/2 + space/2; y < h - margin/2 - size/2; y += space) {
+                var n = 0;
+                for (var x = component.width/2 + space/2; x < component.width - margin/2 - size/2; x += space) {
+                    graphic.push();
+                    graphic.translate(x, y);
+                    graphic.rect(0, 0, size, size),
+                    graphic.pop();
+
+                    graphic.push();
+                    graphic.translate(component.width/2 - space/2 - space * n, y);
+                    graphic.rect(0, 0, size, size),
+                    graphic.pop();
+
+                    graphic.push();
+                    graphic.translate(x, h/2 - space/2 - space * m);
+                    graphic.rect(0, 0, size, size),
+                    graphic.pop();
+
+                    graphic.push();
+                    graphic.translate(component.width/2 - space/2 - space * n, h/2 - space/2 - space * m);
+                    graphic.rect(0, 0, size, size),
+                    graphic.pop();
+
+                    n++;
+                }
+                m++;
+            }
+            graphic.pop();
+        },
+        vBands: function(component, graphic) {
+            var xmargin = p.ceil(component.width * (p.random(1, 20)/100)),
+                ymargin = p.ceil(component.height * (p.random(1, 20)/100)),
+                w = component.width - xmargin,
+                cols = Math.floor(p.constrain(p.random(5, 20), 5, w/3)),
+                y1 = component.lowerLeft.y - ymargin/2,
+                y2 = y1 - component.height + ymargin;
+            graphic.stroke(scale(0.5).darken().rgb());
+            graphic.strokeWeight(3);
+            // graphic.rect(component.lowerLeft.x + xmargin/2, component.lowerLeft.y - ymargin/2, component.width - xmargin, -component.height + ymargin);
+            for (var i = 0; i < cols - 1; i++) {
+                var x = component.lowerLeft.x + xmargin/2 + w/cols + (w/cols) * i;
+                graphic.line(x, y1, x, y2);
+            }
+        },
+        hBands: function(component, graphic) {
+            var xmargin = p.ceil(component.width * (p.random(1, 20)/100)),
+                ymargin = p.ceil(component.height * (p.random(1, 20)/100)),
+                h = component.height - ymargin,
+                rows = Math.floor(p.constrain(p.random(5, 20), 5, h/3)),
+                x1 = component.lowerLeft.x + xmargin/2,
+                x2 = x1 + component.width - xmargin;
+            graphic.stroke(scale(0.5).darken().rgb());
+            graphic.strokeWeight(3);
+            // graphic.rect(component.lowerLeft.x + xmargin/2, component.lowerLeft.y - ymargin/2, component.width - xmargin, -component.height + ymargin);
+            for (var i = 0; i < rows - 1; i++) {
+                var y = component.lowerLeft.y - ymargin/2 - h/rows - (h/rows) * i;
+                graphic.line(x1, y, x2, y);
+            }
+        },
+
+    };
 
     function makeStars() {
         var result = [],
