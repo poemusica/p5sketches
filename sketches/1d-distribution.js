ab54a1e2f2c1867e38587b1166b26aec42eac9f8
Heather Mahan <heatheramahan@gmail.com>
2016-02-23 17:52:04 -0800

    Third and halve starts compared.

--- sketches/1d-distribution.js
+++ sketches/1d-distribution.js
@@ -14,11 +14,11 @@ var sketch = function (p) {
     p.draw = function () {
         p.background(255);
         p.line(0, p.height * 0.25, p.width, p.height * 0.25);
-        // p.line(0, p.height * 0.5, p.width, p.height * 0.5);
+        p.line(0, p.height * 0.5, p.width, p.height * 0.5);
         // p.line(0, p.height * 0.75, p.width, p.height * 0.75);
         // p.line(0, p.height, p.width, p.height);
-        drawRects(0, p.height * 0.25, 0.85, distributor);
-        // drawRects(p.height * 0.25, p.height * 0.5, 0.8, d2);
+        drawRects(0, p.height * 0.25, 0.8, distributor);
+        drawRects(p.height * 0.25, p.height * 0.5, 0.8, d2);
         // drawRects(p.height * 0.5, p.height * 0.75, 0.75, d3);
         // drawRects(p.height * 0.75, p.height, 0, d4);
         p.noLoop();
@@ -47,7 +47,7 @@ var sketch = function (p) {
                 continue;
             }
             p.stroke(0);
-            // p.line(x, y, x, h);
+            p.line(x, y, x, h);
             if (Math.random() < prob) {
                 p.fill(p.random(0, 255));
                 p.rect(x, h - height/2, width, height);
@@ -57,33 +57,31 @@ var sketch = function (p) {
     }
 
     function makeDistributor2(w) {
-        var divisor = 6,
+        var divisor = 3,
             xDivs = [],
             start = 0,
-            sign = -1,
-            j = 1,
-            end = 0;
-        return function () {
+            end = -1,
+            j = 0;
+        return function() {
             var x;
-            if (xDivs.length < 2) {
-                x = w/2 + (w/divisor) * sign;
-                xDivs.push(x);
-                sign *= -1;
-            } else {
-                var prev = xDivs[j],
-                    space = w/divisor * sign;
-                x = prev + space;
-                xDivs.push(x);
-                if (sign > 0) { j--; }
-                sign *= -1;
+            if (xDivs.length === 0) {
+                x = w/divisor;
             }
+            else if (j == end) {
+                x = w - w/divisor;
+            }
+            else {
+                x = xDivs[j] - w/divisor;
+            }
+            xDivs.push(x);
+            j--;
             if (j < end) {
-                end = xDivs.length - divisor/2;
+                end = -1;
                 start = xDivs.length - 1;
-                divisor *= 2;
                 j = start;
+                divisor *= 2;
             }
-            // console.log(x, j, start, end, divisor, sign, xDivs);
+            console.log(x, j, start, end, divisor, xDivs);
             return x;
         };
     }
