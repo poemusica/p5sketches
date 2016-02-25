87b7ec80c31944a190961150f97123e466560eb2
Heather Mahan <heatheramahan@gmail.com>
2016-02-24 17:03:44 -0800

    Code for lines with or without outer margin and padding.

--- sketches/grid-maker.js
+++ sketches/grid-maker.js
@@ -5,7 +5,7 @@ var sketch = function (p) {
     };
 
     p.draw = function () {
-        p.background(0);
+        p.background(255);
         p.fill(175);
         var data = makeData(new p5.Vector(100, 100)),
             width = data.width - 2 * data.margin.x - 2 * data.padding.x,
@@ -14,14 +14,14 @@ var sketch = function (p) {
         p.rect(data.loc.x, data.loc.y, data.width, data.height);
         drawRects(grid, data);
 
-        var data = makeVertData(new p5.Vector(400, 100)),
+        var data = makeVertData(new p5.Vector(300, 100)),
              width = data.width - 2 * data.margin.x - 2 * data.padding.x,
              height = data.height - 2 * data.margin.y - 2 * data.padding.y,
              grid = makeGrid(data.rows, data.cols, width, height);
         p.rect(data.loc.x, data.loc.y, data.width, data.height);
         drawVerticals(grid, data);
 
-        var data = makeHorizData(new p5.Vector(800, 100)),
+        var data = makeHorizData(new p5.Vector(500, 100)),
              width = data.width - 2 * data.margin.x - 2 * data.padding.x,
              height = data.height - 2 * data.margin.y - 2 * data.padding.y,
              grid = makeGrid(data.rows, data.cols, width, height);
@@ -77,10 +77,10 @@ var sketch = function (p) {
         data.margin = p.createVector(0, 0);
         var xpaddingLimit = (data.width - 2 * data.margin.x)/data.cols,
             ypaddingLimit = (data.height - 2 * data.margin.y)/data.rows;
-        data.padding = p.createVector(p.random(5, xpaddingLimit * 0.25), 5);
+        data.padding = p.createVector(p.random(2, xpaddingLimit * 0.25), 2);
 
-        while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 10) { data.cols--; }
-        while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 10) { data.rows--; }
+        while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 5) { data.cols--; }
+        while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 5) { data.rows--; }
         return data;
     }
 
@@ -95,10 +95,10 @@ var sketch = function (p) {
         data.margin = p.createVector(0, 0);
         var xpaddingLimit = (data.width - 2 * data.margin.x)/data.cols,
             ypaddingLimit = (data.height - 2 * data.margin.y)/data.rows;
-        data.padding = p.createVector(5, p.random(5, ypaddingLimit * 0.25));
+        data.padding = p.createVector(2, p.random(2, ypaddingLimit * 0.25));
 
-        while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 10) { data.cols--; }
-        while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 10) { data.rows--; }
+        while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 5) { data.cols--; }
+        while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 5) { data.rows--; }
         return data;
     }
 
@@ -119,13 +119,18 @@ var sketch = function (p) {
         var offset = p5.Vector.add(data.loc, data.margin);
         offset.add(data.padding);
         p.push();
+        p.strokeJoin(p.BEVEL);
         p.stroke(0);
         p.translate(offset.x, offset.y);
         for (var i = 0; i < grid.grid.length; i++) {
             var cell = grid.grid[i];
             p.push();
-            p.translate(cell.x + grid.cellWidth/2, cell.y + data.padding.y);
-            p.line(0, 0, 0, grid.cellHeight - 2 * data.padding.y);
+            //Without outer margin and padding
+            p.translate(cell.x + grid.cellWidth/2, cell.y - data.padding.y);
+            p.line(0, 0, 0, grid.cellHeight + data.padding.y * 2);
+            // With outer margin and padding
+            // p.translate(cell.x + grid.cellWidth/2, cell.y + data.padding.y);
+            // p.line(0, 0, 0, grid.cellHeight - 2 * data.padding.y);
             p.pop();
         }
         p.pop();
@@ -135,13 +140,18 @@ var sketch = function (p) {
         var offset = p5.Vector.add(data.loc, data.margin);
         offset.add(data.padding);
         p.push();
+        p.strokeJoin(p.BEVEL);
         p.stroke(0);
         p.translate(offset.x, offset.y);
         for (var i = 0; i < grid.grid.length; i++) {
             var cell = grid.grid[i];
             p.push();
-            p.translate(cell.x + data.padding.x, cell.y + grid.cellHeight/2);
-            p.line(0, 0, grid.cellWidth - 2 * data.padding.x, 0);
+            // Without outer margin and padding
+            p.translate(cell.x - data.padding.x, cell.y + grid.cellHeight/2);
+            p.line(0, 0, grid.cellWidth + data.padding.x * 2, 0);
+            // With outer margin and padding
+            // p.translate(cell.x + data.padding.x, cell.y + grid.cellHeight/2);
+            // p.line(0, 0, grid.cellWidth - 2 * data.padding.x, 0);
             p.pop();
         }
         p.pop();
