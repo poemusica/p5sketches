eccba31c7ed19e3526a16d2746b1e2be2e46679d
Heather Mahan <heatheramahan@gmail.com>
2016-02-24 16:19:45 -0800

    Regular rectangles, horizontal lines, vertical lines.

--- sketches/grid-maker.js
+++ sketches/grid-maker.js
@@ -6,14 +6,29 @@ var sketch = function (p) {
 
     p.draw = function () {
         p.background(0);
-        p.stroke(255);
-        p.fill(255);
-        var data = makeData(),
+        p.fill(175);
+        var data = makeData(new p5.Vector(100, 100)),
             width = data.width - 2 * data.margin.x - 2 * data.padding.x,
             height = data.height - 2 * data.margin.y - 2 * data.padding.y,
             grid = makeGrid(data.rows, data.cols, width, height);
         p.rect(data.loc.x, data.loc.y, data.width, data.height);
         drawRects(grid, data);
+
+        var data = makeVertData(new p5.Vector(400, 100)),
+             width = data.width - 2 * data.margin.x - 2 * data.padding.x,
+             height = data.height - 2 * data.margin.y - 2 * data.padding.y,
+             grid = makeGrid(data.rows, data.cols, width, height);
+        p.rect(data.loc.x, data.loc.y, data.width, data.height);
+        drawVerticals(grid, data);
+
+        var data = makeHorizData(new p5.Vector(800, 100)),
+             width = data.width - 2 * data.margin.x - 2 * data.padding.x,
+             height = data.height - 2 * data.margin.y - 2 * data.padding.y,
+             grid = makeGrid(data.rows, data.cols, width, height);
+        p.rect(data.loc.x, data.loc.y, data.width, data.height);
+        drawHorizontals(grid, data);
+
+
         p.noLoop();
     };
 
@@ -21,9 +36,9 @@ var sketch = function (p) {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
     };
 
-    function makeData() {
+    function makeData(pos) {
         var data = {
-            loc: p.createVector(100, 100),
+            loc: pos,
             width: p.random(100, 150),
             height: p.random(100, 200),
             rows: Math.floor(p.random(3, 10)),
@@ -49,18 +64,88 @@ var sketch = function (p) {
         while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 10) { data.cols--; }
         while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 10) { data.rows--; }
         return data;
-    };
+    }
+
+    function makeVertData(pos) {
+        var data = {
+            loc: pos,
+            width: p.random(100, 150),
+            height: p.random(100, 200),
+            rows: 1,
+            cols: Math.floor(p.random(10, 30)),
+        };
+        data.margin = p.createVector(0, 0);
+        var xpaddingLimit = (data.width - 2 * data.margin.x)/data.cols,
+            ypaddingLimit = (data.height - 2 * data.margin.y)/data.rows;
+        data.padding = p.createVector(p.random(5, xpaddingLimit * 0.25), 5);
+
+        while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 10) { data.cols--; }
+        while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 10) { data.rows--; }
+        return data;
+    }
+
+    function makeHorizData(pos) {
+        var data = {
+            loc: pos,
+            width: p.random(100, 150),
+            height: p.random(100, 200),
+            rows: Math.floor(p.random(25, 50)),
+            cols: 1,
+        };
+        data.margin = p.createVector(0, 0);
+        var xpaddingLimit = (data.width - 2 * data.margin.x)/data.cols,
+            ypaddingLimit = (data.height - 2 * data.margin.y)/data.rows;
+        data.padding = p.createVector(5, p.random(5, ypaddingLimit * 0.25));
+
+        while ((data.width - 2 * data.margin.x - 2 * data.padding.x)/data.cols < 10) { data.cols--; }
+        while ((data.height - 2 * data.margin.y - 2 * data.padding.y)/data.rows < 10) { data.rows--; }
+        return data;
+    }
 
     function drawRects(grid, data) {
         var offset = p5.Vector.add(data.loc, data.margin);
         offset.add(data.padding);
-        p.fill(175);
+        p.push();
+        p.fill(255);
         p.translate(offset.x, offset.y);
          for (var i = 0; i < grid.grid.length; i++) {
             var cell = grid.grid[i];
             p.rect(cell.x + data.padding.x, cell.y + data.padding.y, grid.cellWidth - 2 * data.padding.x, grid.cellHeight - 2 * data.padding.y);
         }
-    };
+        p.pop();
+    }
+
+    function drawVerticals(grid, data) {
+        var offset = p5.Vector.add(data.loc, data.margin);
+        offset.add(data.padding);
+        p.push();
+        p.stroke(0);
+        p.translate(offset.x, offset.y);
+        for (var i = 0; i < grid.grid.length; i++) {
+            var cell = grid.grid[i];
+            p.push();
+            p.translate(cell.x + grid.cellWidth/2, cell.y + data.padding.y);
+            p.line(0, 0, 0, grid.cellHeight - 2 * data.padding.y);
+            p.pop();
+        }
+        p.pop();
+    }
+
+    function drawHorizontals(grid, data) {
+        var offset = p5.Vector.add(data.loc, data.margin);
+        offset.add(data.padding);
+        p.push();
+        p.stroke(0);
+        p.translate(offset.x, offset.y);
+        for (var i = 0; i < grid.grid.length; i++) {
+            var cell = grid.grid[i];
+            p.push();
+            p.translate(cell.x + data.padding.x, cell.y + grid.cellHeight/2);
+            p.line(0, 0, grid.cellWidth - 2 * data.padding.x, 0);
+            p.pop();
+        }
+        p.pop();
+    }
 
     function makeGrid(rows, cols, width, height) {
         var grid = {
