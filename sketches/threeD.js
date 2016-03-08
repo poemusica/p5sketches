4a24f192114f724667a12b83c46064c02a8f3221
Heather Mahan <heatheramahan@gmail.com>
2016-03-08 12:01:25 -0800

    WIP. grid on a plane.

--- sketches/threeD.js
+++ sketches/threeD.js
@@ -1,36 +1,44 @@
 var sketch = function (p) {
-    var angle = {
-            x: 0,
-            y: 0,
-            z: 0
-        },
-        position =  {
-            x: 0,
-            y: 0,
-            z: 0
-        },
-        grid;
+    var angle,
+        position,
+        grid,
+        target;
 
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
         grid = makeGrid(10, 10, p.width, p.height);
+        angle = p.createVector();
+        position = p.createVector();
+        target = p.createVector();
+        target.normalize();
     };
 
     p.draw = function () {
         p.background(255);
         p.push();
-        p.translate(-p.width/2, -p.height/2, 0);
+        p.translate(position.x, position.y, position.z);
+        p.rotateX(angle.x);
+        p.rotateY(angle.y);
+        p.rotateZ(angle.z);
+
+
+        p.push();
+        p.translate(0, 25, 0);
+        p.rotateX(p.PI/2);
+        p.plane(grid.width/2, grid.height/2);
+        p.pop();
+
+        p.push();
+        p.translate(-p.width/2, 0, -p.height/2);
         for (var i = 0; i < grid.grid.length; i++) {
             var cell = grid.grid[i];
             p.push();
-            p.translate(cell.x + grid.cellWidth/2, cell.y + grid.cellHeight/2, position.z);
-            p.rotateX(p.radians(angle.x));
-            p.rotateY(p.radians(angle.y));
+            p.translate(cell.x + grid.cellWidth/2, 0, cell.y + grid.cellHeight/2);
             p.box(25, 25, 25);
             p.pop();
         }
         p.pop();
-
+        p.pop();
     };
 
     p.windowResized = function () {
@@ -38,8 +46,24 @@ var sketch = function (p) {
     };
 
     p.mouseDragged = function(event) {
-        angle.x += event.movementY;
-        angle.y += event.movementX;
+        angle.x += p.radians(event.movementY);
+        angle.y += p.radians(event.movementX);
+    };
+
+
+    p.keyPressed = function () {
+      if (p.keyCode === p.DOWN_ARROW) {
+        position.z -= 10;
+      } else if (p.keyCode === p.UP_ARROW) {
+        position.z += 10;
+      } else if (p.keyCode === p.RIGHT_ARROW) {
+        angle.y += p.radians(1);
+        target.rotate(angle.y);
+      } else if (p.keyCode === p.LEFT_ARROW) {
+        angle.y -= p.radians(1);
+        target.rotate(angle.y);
+      }
+      return false;
     };
 
     p.mouseWheel = function(event) {
