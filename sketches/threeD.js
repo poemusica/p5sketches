b00c5a563f58775d7568e2aa87eae2afdf89f765
Heather Mahan <heatheramahan@gmail.com>
2016-06-24 19:40:56 -0700

    sines and threeD updates

--- sketches/threeD.js
+++ sketches/threeD.js
@@ -2,32 +2,39 @@ var sketch = function (p) {
     var angle,
         position,
         grid,
-        target;
+        target,
+        direction;
 
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
+        // p.perspective(fovy,aspect,near,far)
+        p.perspective(60 / 180 * p.PI, p.width/p.height, 0.00001, 100);
         grid = makeGrid(10, 10, p.width, p.height);
         angle = p.createVector();
         position = p.createVector();
-        target = p.createVector();
-        target.normalize();
+        direction = p.createVector(0, 0, -1);
     };
 
     p.draw = function () {
         p.background(255);
+        p.camera(0, 0, -800);
+        p.line(0, 10, 0, 10, 0, -800);
+        
         p.push();
+        
+        // World
         p.translate(position.x, position.y, position.z);
-        p.rotateX(angle.x);
-        p.rotateY(angle.y);
-        p.rotateZ(angle.z);
+        // p.rotateX(angle.x);
+        // p.rotateY(angle.y);
+        // p.rotateZ(angle.z);
 
-
+        // Plane
         p.push();
         p.translate(0, 25, 0);
         p.rotateX(p.PI/2);
         p.plane(grid.width/2, grid.height/2);
         p.pop();
-
+        // Grid
         p.push();
         p.translate(-p.width/2, 0, -p.height/2);
         for (var i = 0; i < grid.grid.length; i++) {
@@ -38,6 +45,7 @@ var sketch = function (p) {
             p.pop();
         }
         p.pop();
+        
         p.pop();
     };
 
@@ -45,31 +53,32 @@ var sketch = function (p) {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
     };
 
-    p.mouseDragged = function(event) {
-        angle.x += p.radians(event.movementY);
-        angle.y += p.radians(event.movementX);
-    };
+    // p.mouseDragged = function(event) {
+    //     angle.x += p.radians(event.movementY);
+    //     angle.y += p.radians(event.movementX);
+    // };
 
 
     p.keyPressed = function () {
-      if (p.keyCode === p.DOWN_ARROW) {
-        position.z -= 10;
-      } else if (p.keyCode === p.UP_ARROW) {
-        position.z += 10;
-      } else if (p.keyCode === p.RIGHT_ARROW) {
-        angle.y += p.radians(1);
-        target.rotate(angle.y);
-      } else if (p.keyCode === p.LEFT_ARROW) {
-        angle.y -= p.radians(1);
-        target.rotate(angle.y);
-      }
+        if (p.keyCode === p.DOWN_ARROW) {
+            position.z -= 10;
+        } else if (p.keyCode === p.UP_ARROW) {
+            position.z += 10;
+        } else if (p.keyCode === p.RIGHT_ARROW) {
+            // angle.y -= p.radians(2);
+            direction.rotate(-2);
+        } else if (p.keyCode === p.LEFT_ARROW) {
+            // angle.y += p.radians(2);
+            direction.rotate(2);
+        }
+
       return false;
     };
 
-    p.mouseWheel = function(event) {
-        position.z += event.delta;
-        return false;
-    };
+    // p.mouseWheel = function(event) {
+    //     position.z += event.delta;
+    //     return false;
+    // };
 
 
     function makeGrid(rows, cols, width, height) {
